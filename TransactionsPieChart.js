89ab5c30a0d2824792c4debb3751b1c89854e9c8
prit <162418331+dhembare-pritam@users.noreply.github.com>
2024-10-16 21:10:01 +0530

    Add files via upload

---
+++ TransactionsPieChart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Register the components
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend
+);
+
+const TransactionsPieChart = ({ data }) => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Transactions Pie Chart',
+      },
+    },
+  };
+
+  return <Pie options={options} data={data} />;
+};
+
+export default TransactionsPieChart;
